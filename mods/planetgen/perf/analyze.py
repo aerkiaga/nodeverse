30f1b0c91d07bc2532c3b54346ddc891affd4b6d
Aritz Erkiaga <aerkiaga3@gmail.com>
2021-08-23 06:17:07 +0200

    Improve 'analyze.py'

--- mods/planetgen/perf/analyze.py
+++ mods/planetgen/perf/analyze.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
-import sys, re
+import sys, re, math
 
 re_line = re.compile(r"(\w+) (\d+)")
 
 data = {}
+
+def time_format(n):
+    r = None
+    if n > 0.5e+6:
+        r = "{:.2f} s".format(n/1e+6)
+    elif n > 0.5e+3:
+        r = "{:.2f} ms".format(n/1e+3)
+    else:
+        r = "{:.2f} us".format(n)
+    return r
 
 for line in sys.stdin:
     match = re_line.match(line)
@@ -18,6 +28,10 @@ for line in sys.stdin:
             }
         data[name]["count"] += 1
         data[name]["total"] += time
-print("NAME" + (32-4)*" " + "COUNT" + (12-5)*" " + "TOTAL")
-for name, stats in data.items():
-    print("{:<32s}{:<12d}{:d}".format(name, stats["count"], stats["total"]))
+print("NAME" + (32-4)*" " + "COUNT" + (12-5)*" " +\
+"AVERAGE" + (12-7)*" " + "TOTAL" + (12-5)*" " + "...")
+data = sorted(data.items(), key=lambda x: x[1]["total"], reverse=True)
+for name, stats in data:
+    print("{:<32s}{:<12d}{:<12s}{:<12s}{:s}".format(name, stats["count"],\
+    time_format(stats["total"]/stats["count"]), time_format(stats["total"]),
+    math.floor(math.log2(stats["total"]))*"#"))
