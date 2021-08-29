f0f92b66b4b5a8326975895fbf2432686c7caa82
Aritz Erkiaga <aerkiaga3@gmail.com>
2021-08-30 01:16:15 +0200

    Add basic seat model

--- mods/nv_ship_building/control.lua
+++ mods/nv_ship_building/control.lua
@@ -113,25 +113,28 @@ end
 
 minetest.register_node("nv_ship_building:seat", {
     description = "Seat",
-    drawtype = "normal",
+    drawtype = "mesh",
     sunlight_propagates = true,
     paramtype2 = "facedir",
 
     tiles = {"seat.png"},
     use_texture_alpha = "clip",
     groups = { oddly_breakable_by_hand=3 },
+    mesh = "seat.obj",
 
     on_rightclick = ship_rightclick_callback,
 })
 
 minetest.register_entity("nv_ship_building:ent_seat", {
     initial_properties = {
-        visual = "cube",
+        visual = "mesh",
         textures = {
             "seat.png", "seat.png", "seat.png",
             "seat.png", "seat.png", "seat.png"
         },
-        use_texture_alpha = true
+        use_texture_alpha = true,
+        visual_size = {x=10, y=10, z=10},
+        mesh = "seat.obj"
     },
 })
 
