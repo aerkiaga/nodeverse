7dc7abe34e532f52d0fd55366dec886f3ce8e4dd
Aritz Erkiaga <aerkiaga3@gmail.com>
2021-09-05 17:21:42 +0200

    Finish fixing death/damage on landing

--- mods/nv_ships/nodetypes.lua
+++ mods/nv_ships/nodetypes.lua
@@ -20,6 +20,7 @@ local function register_node_and_entity(name, def)
         mesh = def.mesh,
         selection_box = def.collision_box,
         collision_box = def.collision_box,
+        groups = def.groups,
         after_place_node = after_place_node_normal,
         on_rightclick = nv_ships.ship_rightclick_callback,
     }
@@ -56,6 +57,9 @@ register_node_and_entity("seat", {
         fixed = {
             {-0.5, -0.5, -0.5, 0.5, 0, 0.5}
         },
+    },
+    groups = {
+        fall_damage_add_percent = -100
     },
 
     visual = "mesh",
