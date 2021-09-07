804aaeb1982cacd3f6db50b22d28d340322866e3
Aritz Erkiaga <aerkiaga3@gmail.com>
2021-09-07 02:58:18 +0200

    Isolate assets from 'player_api'

--- mods/nv_player/init.lua
+++ mods/nv_player/init.lua
@@ -17,10 +17,28 @@ function nv_player.set_collisionbox(player, collisionbox)
     player:set_properties(properties)
 end
 
+-- Default player appearance
+player_api.register_model("character.b3d", {
+	animation_speed = 30,
+	textures = {"spacesuit.png"},
+	animations = {
+		-- Standard animations.
+		stand     = {x = 0,   y = 79},
+		lay       = {x = 162, y = 166},
+		walk      = {x = 168, y = 187},
+		mine      = {x = 189, y = 198},
+		walk_mine = {x = 200, y = 219},
+		sit       = {x = 81,  y = 160},
+	},
+	collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
+	stepheight = 0.6,
+	eye_height = 1.47,
+})
+
 -- Default player model, but all animations are the 'sit' animation
 player_api.register_model("character_sitting.b3d", {
 	animation_speed = 30,
-	textures = {"character.png"},
+	textures = {"spacesuit.png"},
 	animations = {
 		-- 'Sit' animation, everywhere
 		stand     = {x = 81,  y = 160},
