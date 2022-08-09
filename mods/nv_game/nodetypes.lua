5b90fe7bcc9cb7057669adb1f8f8965aedb41205
Aritz Erkiaga <aerkiaga3@gmail.com>
2022-08-09 03:32:14 +0200

    Add loot to piñatas

--- mods/nv_game/nodetypes.lua
+++ mods/nv_game/nodetypes.lua
@@ -2,8 +2,42 @@
 Here are defined any nodes required by the Nodeverse game at the top level.
 
  # INDEX
+    LOOT CALCULATION
     NODE TYPES
 ]]
+
+--[[
+ # LOOT CALCULATION
+]]--
+
+local loot_table = {
+    ["nv_ships:seat"] = 10,
+}
+
+local function compute_pinata_loot_table()
+    local raw_p = 0
+    for name, rarity in pairs(loot_table) do
+        raw_p = raw_p + 1 / rarity
+    end
+    local weighted_loot_table = {}
+    local remaining_p = 1
+    for name, rarity in pairs(loot_table) do
+        weighted_loot_table[name] = rarity * raw_p * remaining_p
+        remaining_p = remaining_p * (1 - 1 / weighted_loot_table[name])
+    end
+    local r = {}
+    local index = 1
+    for name, rarity in pairs(weighted_loot_table) do
+        r[index] = {}
+        r[index].items = {name}
+        r[index].rarity = rarity
+        print(name .. " " .. rarity)
+    end
+    return {
+        max_items = 1,
+        items = r,
+    }
+end
 
 --[[
  # NODE TYPES
@@ -32,4 +66,6 @@ minetest.register_node("nv_game:pinata", {
             {-3/16, -0.5, -0.5, 3/16, 0.5, 0.5}
         },
     },
+    
+    drop = compute_pinata_loot_table(),
 })
