a83797d5e9d4ee2939a6d899b8c02a928356278e
Aritz Erkiaga <aerkiaga3@gmail.com>
2023-08-03 23:43:49 +0200

    Rebalance cliff parameters

--- mods/nv_planetgen/meta.lua
+++ mods/nv_planetgen/meta.lua
@@ -93,7 +93,7 @@ function generate_planet_metadata(seed)
     
     -- CLIFF ALTITUDE PERIOD
     -- Cliffs will repeat every multiple of this altitude
-    planet.cliff_altitude_period = 2^gen_linear(G, 0.5, 8)
+    planet.cliff_altitude_period = (2^gen_linear(G, 0.5, 4))^2
     return planet
 end
 
